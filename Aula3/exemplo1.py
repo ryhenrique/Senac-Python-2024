1e5737e8d85e28042e1dd85dc601c6b0a89cf92d
Ryan Henrique <ryanhenriquesilvaa@outlook.com>
2024-10-02 19:52:05 -0300

    Cálculo sobre imposto
    Código sobre calcular o imposto de um produto!

---
+++ Aula3/exemplo1.py
@@ -0,0 +1,4 @@
+print("Vamos calcular o imposto!")
+valorproduto = float(input("Coloque o valor do produto:"))
+imposto = valorproduto * 0.1
+print(f"O valor que você erá que pagar do imposto desse produto é {imposto}")
