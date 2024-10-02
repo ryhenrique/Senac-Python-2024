35697061ba756819cb4e2b741e8464564cb4346b
Ryan Henrique <ryanhenriquesilvaa@outlook.com>
2024-10-02 19:32:06 -0300

    Exemplo 1 atualização
    Melhorei o mapa do tesouro

--- Aula2/ex1.py
+++ Aula2/ex1.py
@@ -1,4 +1,6 @@
 #Esses serão os passos do pirata
+print("Esse é o caminho para o mapa do tesouro")
 print("Andar para direita")
 print("Andar para esquerda")
 print("Andar para baixo")
+print("Procure o X e cave")
