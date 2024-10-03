2f85f4050749f9c3af03579335ef4730c801b4c8
Ryan Henrique <ryanhenriquesilvaa@outlook.com>
2024-10-02 21:45:19 -0300

    Cálculo sobre medidas do cachorro
    Exercício de medidas de tamanho do sapato

---
+++ Aula3/ex3.py
@@ -0,0 +1,7 @@
+print("Vamos calcular o tamanho!")
+coleira = int(input("Qual é o tamanho da coleira?"))
+funcieira = int(input("Qual é o tamanho da funcieira?"))
+sapato = coleira*2
+funcieira = coleira*3
+print(f"O tamanho do sapato é: {sapato}")
+print(f"O tamanho da funcieira é: {funcieira}")
