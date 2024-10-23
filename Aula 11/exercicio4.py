10de785398fc266e17827231c1ece04d0c68c074
Ryan Henrique <ryanhenriquesilvaa@outlook.com>
2024-10-23 02:20:06 -0300

    Dever de Casa - Aula 11
    Finalizado em casa + adicionar outros codigos adicionais no exercício4.py

--- Aula 11/exercicio4.py
+++ Aula 11/exercicio4.py
@@ -1,7 +1,23 @@
+ # Conveter Celsius em Fahrenheit
 
 def converter(celsius):
     return (celsius * 9 / 5 + 32)
 
-celsius = float(input("Digite sua temperatura em Celsius: "))
+celsius = float(input("Digite sua temperatura em Graus Celsius: "))
 resultado = converter(celsius)
-print(f"A temperatura de {celsius}°C é equivalente a {resultado}°F")
+print(f"A temperatura de {celsius}° Celsius é equivalente a {resultado}° Fahrenheit")
+
+# Converter Celsius em Kelvin
+def converter(celsiustokelvin):
+    return(celsiustokelvin + 273.15)
+celsiustokelvin = float(input("Digite sua temperatura em Graus Celsius: "))
+resultado = converter(celsiustokelvin)
+print(f"a temperatura de {celsiustokelvin}° Celsius é equivalente a: {resultado} Kelvin")
+
+# Converter Fahrenheit em Kelvin
+
+def converter(fahrenheit):
+    return(fahrenheit - 32) * 5/9 + 273.15
+fahrenheit = float(input("Digite sua temperatura em ° Fahrenheit: "))
+resultado = converter(fahrenheit)
+print(f"A sua temperatura de {fahrenheit}° Fahrenheit é equivalente a: {resultado} Kelvin")
